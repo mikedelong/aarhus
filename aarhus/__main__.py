68639d9c062ea8879d63105f4936493e60c934c3
Mike DeLong <mikedelong@outlook.com>
2016-12-14 21:01:30 -0500

    feature: Overall timing.
    We want to know how long everything took, so we have timing code at the outermost.
    
    Initial checkin.

--- aarhus/__main__.py
+++ aarhus/__main__.py
@@ -1,6 +1,7 @@
 
 import json
 import logging
+import time
 
 
 class Importer(object):
@@ -12,6 +13,8 @@ class Importer(object):
 
 
 def run():
+    start_time = time.time()
+
     with open('settings.json') as data_file:
         data = json.load(data_file)
         logging.debug(data)
@@ -19,6 +22,13 @@ def run():
 
     instance = Importer(input_folder)
     pass
+
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logging.info(
+        "Elapsed time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+
 
 if __name__ == '__main__':
     run()
