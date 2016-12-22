ec3eb9a4a4a2fba0b2fdaa56c93e2dd4b83f981a
Mike DeLong <mikedelong@outlook.com>
2016-12-22 12:13:24 -0500

    fix: all random_states use 0
    The original example used a random_state of 42; we are using 0 everywhere.
    
    ...

--- demos/newsgroups.py
+++ demos/newsgroups.py
@@ -10,7 +10,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
 categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=0)
 
 print("\n".join(twenty_train.data[0].split("\n")[:3]))
 print(twenty_train.target_names[twenty_train.target[0]])
