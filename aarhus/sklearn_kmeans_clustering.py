04d83cac942f8eaceb9a82c8af2b30d42bfc65da
Mike DeLong <mikedelong@outlook.com>
2017-01-08 19:49:00 -0500

    feature: tfidf min_df and max_df are now settings
    We've also added minimal support for multi-dimensional scaling and Ward multilevel clustering,
    but the multilevel clustering is running into memory issues so it is currently inactive.
    Finally, we're using the accessor for the specific stopwords to refine our bag of words.
    This is not a great solution; it is prone to overfitting.
    
    ...

--- aarhus/sklearn_kmeans_clustering.py
+++ aarhus/sklearn_kmeans_clustering.py
@@ -4,12 +4,14 @@ import os
 import re
 import string
 from collections import Counter
+from scipy.cluster.hierarchy import ward, dendrogram
 
 import nltk
-import pyzmail
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import MDS
+from sklearn.metrics.pairwise import cosine_similarity
 
 from aarhus.aarhus import custom_stopwords
 
@@ -23,9 +25,9 @@ def strip_proppers(text):
     tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)
               if word.islower()]
     return "".join(
-        [" " + i if not i.startswith("'") and not i.startswith("/") and not i.endswith(
-            "=") and i not in string.punctuation else i for i in tokens]).strip()
+        [" " + i if not i.startswith("'") and i not in string.punctuation else i for i in tokens]).strip()
 
+touchup_list = custom_stopwords.get_specific_stopwords()
 
 def tokenize_and_stem(text):
     # first tokenize by sentence, then by word to ensure that punctuation is caught as it'sown token
@@ -36,10 +38,14 @@ def tokenize_and_stem(text):
         if re.search('[a-zA-Z]', token):
             filtered_tokens.append(token)
     stems = [stemmer.stem(t) for t in filtered_tokens]
-    return stems
+    # todo remove touchup list ?
+    result = [stem for stem in stems if stem not in touchup_list]
+    return result
+
 
 # https://groups.google.com/forum/#!topic/microsoft.public.outlookexpress.general/oig7-xNFISg
 clean_address_tokens = ['=?us-ascii?Q?', '=0D=0A_=28', '=?utf-8?Q?', '=29?=', '=0D=0A']
+
 
 def clean_address(arg_value):
     result = str(arg_value)
@@ -57,43 +63,12 @@ with open('./sklearn_kmeans_clustering.json') as data_file:
     # model_file_name = data['model_file_name']
     cluster_count = data['cluster_count']
     random_state = data['random_state']
+    max_df = data['max_df']
+    min_df = data['min_df']
 
 target_encoding = 'utf-8'
-def filter_file_by_content(arg_file_name, arg_senders):
-    with open(arg_file_name, 'rb') as fp:
-        message = pyzmail.message_from_file(fp)
-        # todo clean up internal whitespace
-        senders = message.get_addresses('from')
-        clean_senders = [clean_address(item[1]) for item in senders]
-
-        if len(set(clean_senders) & arg_senders) != 0:
-            return None
-
-        text_part = message.text_part
-        if text_part is not None:
-            charset = text_part.charset
-            payload = text_part.get_payload()
-            if charset is not None:
-                try:
-                    body = payload.decode(charset, 'ignore').encode(target_encoding)
-                except LookupError as lookupError:
-                    if text_part.charset == 'iso-8859-8-i':
-                        body = payload.decode('iso-8859-8', 'ignore').encode(target_encoding)
-                    else:
-                        body = payload.decode('utf-8', 'ignore').encode(target_encoding)
-                        logging.warn('lookup error %s', lookupError)
-            else:
-                body = payload.decode('utf-8', 'ignore').encode(target_encoding)
-
-            body_ascii = body.decode('utf-8', 'ignore').encode('ascii', 'ignore')
-
-            return body_ascii
-        else:
-            return None
 
 stopwords = custom_stopwords.get_stopwords()
-unanalyzed_senders = custom_stopwords.get_unanalyzed_senders()
-unanalyzed_senders = set(unanalyzed_senders)
 
 file_names = [os.path.join(root, current) for root, subdirectories, files in os.walk(input_folder) for current in files]
 logging.debug('we have %d files', len(file_names))
@@ -102,19 +77,26 @@ if max_file_count < len(file_names) and max_file_count != -1:
     file_names = file_names[:max_file_count]
 logging.debug('we are using %d files', len(file_names))
 
-documents = [filter_file_by_content(file_name, unanalyzed_senders) for file_name in file_names]
-documents = [item for item in documents if item is not None]
+documents = [open(file_name, 'r').read() for file_name in file_names]
 
-logging.debug('after sender filtering we have %d files', len(documents))
-
-tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000, min_df=0.2, stop_words='english', use_idf=True,
-                                   tokenizer=tokenize_and_stem, ngram_range=(1, 3))
+tfidf_vectorizer = TfidfVectorizer(max_df=max_df, max_features=200000, min_df=min_df, stop_words='english',
+                                   use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1, 2), decode_error='ignore',
+                                   strip_accents='ascii')
 
 tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
 
 model = KMeans(n_clusters=cluster_count, random_state=random_state)
 
 model.fit(tfidf_matrix)
+
+# todo write terms to a file for later viewing
+terms = tfidf_vectorizer.get_feature_names()
+logging.debug('we have %d terms/feature names' % len(terms))
+terms_out_file = 'sklearn_kmeans_terms.csv'
+with open(terms_out_file, 'w') as terms_out_fp:
+    for item in terms:
+      terms_out_fp.write("%s\n" % item)
+logging.debug('wrote terms to %s' % terms_out_file)
 
 clusters = model.labels_.tolist()
 
@@ -133,5 +115,44 @@ for item in zip(file_names, clusters):
     result[short_file_name] = item[1]
     pass
 
-with open('sklearn_kmeans_clusters.json', 'w') as fp:
+cluster_out_file ='sklearn_kmeans_clusters.json'
+with open(cluster_out_file, 'w') as fp:
     json.dump(result, fp)
+logging.debug('wrote clusters to %s' % cluster_out_file)
+
+if False:
+    distances = 1 - cosine_similarity(tfidf_matrix)
+    logging.debug('computed distances usng cosine similarity')
+
+    MDS()
+
+    # convert two components as we're plotting points in a two-dimensional plane
+    # "precomputed" because we provide a distance matrix
+    # we will also specify `random_state` so the plot is reproducible.
+    mds = MDS(n_components=2, dissimilarity="precomputed", random_state=random_state)
+
+    pos = mds.fit_transform(distances)  # shape (n_components, n_samples)
+
+
+    linkage_matrix = ward(distances)  # define the linkage_matrix using ward clustering pre-computed distances
+    logging.debug('got linkage matrix')
+
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(figsize=(30, 40))  # set size
+    ax = dendrogram(linkage_matrix, orientation="right")  # ), labels=titles);
+
+    plt.tick_params( \
+        axis='x',  # changes apply to the x-axis
+        which='both',  # both major and minor ticks are affected
+        bottom='off',  # ticks along the bottom edge are off
+        top='off',  # ticks along the top edge are off
+        labelbottom='off')
+
+    plt.tight_layout()  # show plot with tight layout
+
+    # uncomment below to save figure
+    plt.savefig('ward_clusters.png', dpi=200)  # save figure as ward_clusters
+
+pass
