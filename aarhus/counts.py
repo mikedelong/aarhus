209a8e9b090394067355511618084926f0b55cd4
Mike DeLong <mikedelong@outlook.com>
2017-01-16 11:51:28 -0500

    feature: fleshed out to visualize using tSNE
    Currently separate the messages with no reply header and visualize them using tSNE. Definitely a work in progress.
    
    ...

--- aarhus/counts.py
+++ aarhus/counts.py
@@ -1,14 +1,35 @@
 import json
 import logging
 import os
+import re
 import sys
 import time
 
+import matplotlib.pyplot as pyplot
+import nltk
 import pyzmail
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import TSNE
 
 # http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
 reload(sys)
 sys.setdefaultencoding("utf8")
+
+stemmer = SnowballStemmer("english")
+
+
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it'sown token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    result = [stemmer.stem(t) for t in filtered_tokens]
+    return result
 
 
 class Importer(object):
@@ -21,13 +42,18 @@ class Importer(object):
         self.process_html_part = arg_process_html_part
         self.process_both_empty = arg_process_both_empty
 
+    def get_if_is_reply(self, arg_file):
+        references = self.get_references(arg_file)
+
     def process_folder(self, arg_folder):
+        not_reply_result = []
         document_count = 0
         no_references_count = 0
         references_count = 0
         message_id_count = 0
         for root, subdirectories, files in os.walk(arg_folder):
             for current in files:
+                # first get the references node
                 if document_count < self.document_count_limit:
                     current_full_file_name = os.path.join(root, current)
                     if document_count % 1000 == 0 and document_count > 0:
@@ -40,8 +66,13 @@ class Importer(object):
                     document_count += 1
                     if references.has_key('message-id'):
                         message_id_count += 1
+                    # if a file isn't a reply let's put it in the not-reply result
+                    if not references.has_key('in-reply-to'):
+                        not_reply_result.append(current)
+
         logging.info('documents : %d message-id: %d references: %d no references: %d' % (
-        document_count, message_id_count, references_count, no_references_count))
+            document_count, message_id_count, references_count, no_references_count))
+        return not_reply_result
 
     target_encoding = 'utf-8'
 
@@ -67,16 +98,31 @@ class Importer(object):
                 result['message-id'] = message['Message-id']
             else:
                 logging.warn('no message id in file %s', current_file)
+                logging.info([key for key in message.keys()])
             if 'References' in message.keys():
                 references = message['References'].split(' ')
                 result['references'] = references
+            if 'In-Reply-To' in message.keys():
+                result['in-reply-to'] = message['In-Reply-To']
         return result
+
+
+def convert(arg_input_folder, arg_limit, arg_filter):
+    document_count = 0
+    result = []
+    for root, subdirectories, files in os.walk(arg_input_folder):
+        for current in files:
+            document_count += 1
+            if document_count < arg_limit and current in arg_filter:
+                current_full_file_name = os.path.join(root, current)
+                result.append(current_full_file_name)
+    return result
 
 
 def run():
     start_time = time.time()
 
-    with open('real-settings.json') as data_file:
+    with open('counts-settings.json') as data_file:
         data = json.load(data_file)
         logging.debug(data)
         input_folder = data['input_folder']
@@ -86,16 +132,48 @@ def run():
         process_text_part = data['process_text_part']
         process_html_part = data['process_html_part']
         process_both_empty = data['process_both_empty']
+        text_input_folder = data['text_input_folder']
+        random_state = data['random_state']
+        max_df = data['max_df']
+        min_df = data['min_df']
+        max_features = data['max_features']
+        n_components = data['n_components']
 
     instance = Importer(arg_document_count_limit=document_count_limit, arg_process_text_part=process_text_part,
                         arg_process_html_part=process_html_part, arg_process_both_empty=process_both_empty)
-    instance.process_folder(input_folder)
+
+    not_reply = instance.process_folder(input_folder)
+    logging.info(not_reply)
+    logging.info(len(not_reply))
+
+    # let's take this sample and build a cluster and display it
+    file_names = convert(text_input_folder, document_count_limit, not_reply)
+
+    documents = [open(file_name, 'r').read() for file_name in file_names]
+
+    tfidf_vectorizer = TfidfVectorizer(max_df=max_df, max_features=max_features, min_df=min_df, stop_words='english',
+                                       use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1, 2),
+                                       decode_error='ignore', strip_accents='ascii')
+
+    tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+
+    X_reduced = TruncatedSVD(n_components=n_components, random_state=random_state).fit_transform(tfidf_matrix)
+
+    X_embedded = TSNE(n_components=2, perplexity=40, verbose=2).fit_transform(X_reduced)
+
+    # fig = pyplot.figure(figsize=(10, 10))
+    ax = pyplot.axes(frameon=False)
+    pyplot.setp(ax, xticks=(), yticks=())
+    pyplot.subplots_adjust(left=0.0, bottom=0.0, right=1.0, top=0.9,wspace=0.0, hspace=0.0)
+    pyplot.scatter(X_embedded[:, 0], X_embedded[:, 1], marker="x")
 
     finish_time = time.time()
     elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
     elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
-    logging.info(
-        "Elapsed time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    logging.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+
+    pyplot.show()
+
 
 
 if __name__ == '__main__':
