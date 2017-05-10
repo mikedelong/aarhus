2e696cf366bf13fc530caa9e43784a8151b9d26f
Mike DeLong <mikedelong@outlook.com>
2017-05-09 20:46:08 -0400

    feature: added an input filename; also a list of stopwords.

--- aarhus/frequencies.py
+++ aarhus/frequencies.py
@@ -1,17 +1,29 @@
 import logging
 import collections
 import sys
+import textract
+import json
 
 # http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
 reload(sys)
 sys.setdefaultencoding("utf8")
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s :: %(message)s', level=logging.DEBUG)
+stop_words = ['of', 'and', 'in', 'the', 'which', 'as', 'to', 'by', 'be', 'is', 'was', 'were', 'on', 'new', 'it', 'had',
+              'for', 'would', 'from', 'an', 'been', 'not', 'this']
+with open('frequencies-settings.json') as data_file:
+    data = json.load(data_file)
+    logging.debug(data)
+    input_file = data['input_file']
 
-text = 'the quick brown fox jumped over the lazy hound dog'
+text = textract.process(input_file)
 
-words = text.split()
+words = [word for word in text.lower().split() if len(word) > 1 and word not in stop_words]
+
+logging.debug(len(words))
 
 counts = collections.Counter(words)
 
 logging.debug(counts)
+
+logging.debug(len(counts))
