c0b42c9a977195a13d542479efc2e23321baafaf
Mike DeLong <mikedelong@outlook.com>
2017-05-21 11:51:46 -0400

    feature: log the most popular tokens in each document as analysis progresses.

--- aarhus/frequencies.py
+++ aarhus/frequencies.py
@@ -64,6 +64,9 @@ if input_folder is not None:
             logging.debug('before stemming we have %d words' % len(current_words))
             current_words = [stemmer.stem(word) for word in current_words]
             logging.debug('after stemming we have %d words' % len(current_words))
+            current_counts = collections.Counter(current_words)
+            current_most = current_counts.most_common(most_count)
+            logging.debug(current_most)
             words.extend(current_words)
             file_count += 1
 
