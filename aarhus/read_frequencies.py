acd5b0cfe91999d6e7367552026ee9a9dd785c2f
Mike DeLong <mikedelong@outlook.com>
2017-05-27 18:05:19 -0400

    cleanup; no functional changes.

--- aarhus/read_frequencies.py
+++ aarhus/read_frequencies.py
@@ -1,4 +1,3 @@
-
 import logging
 import pickle
 
@@ -6,7 +5,6 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s :: %(message)s', level=l
 
 logging.debug('started.')
 input_file = './most_common.pickle'
-data = None
 with open(input_file, 'rb') as input_fp:
     data = pickle.load(input_fp)
 
