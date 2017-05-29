8f46adcb495c79356a9be77e1e0feb1830ac7db4
Mike DeLong <mikedelong@outlook.com>
2017-05-29 08:46:11 -0400

    feature: moved difference code here.

--- aarhus/read_frequencies.py
+++ aarhus/read_frequencies.py
@@ -12,3 +12,25 @@ logging.debug('read preprocessed data from  %s.' % input_file)
 
 for key in data.keys():
     logging.debug('input data has key %s' % key)
+
+file_names = list()
+if 'file_names' in data.keys():
+    file_names = data['file_names']
+
+most = list()
+if 'most_common_from_corpus' in data.keys():
+    most = data['most_common_from_corpus']
+
+per_file_most = list()
+if 'most_common_from_documents' in data.keys():
+    per_file_most = data['most_common_from_documents']
+
+if True:
+    for index, item in enumerate(most):
+        logging.debug('%s: %s :: %d' % (index + 1, item[0], item[1]))
+    # logging.debug('%d unique words/tokens' % len(counts))
+    most_words = set([each[0] for each in most])
+    logging.debug(most_words)
+    for item in per_file_most:
+        current = set([each[0] for each in item])
+        logging.debug(current.difference(most))
