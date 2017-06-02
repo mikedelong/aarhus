d47d71b197742eaf3092bc605fd59eba1e1879ef
Mike DeLong <mikedelong@outlook.com>
2017-06-02 18:35:02 -0400

    feature: added a count of the difference between the document most common and the corpus most common.

--- aarhus/read_frequencies.py
+++ aarhus/read_frequencies.py
@@ -25,14 +25,16 @@ counts_from_documents = list()
 if 'counts_from_documents' in data.keys():
     counts_from_documents = data['counts_from_documents']
 
-t0 = counts_from_corpus.most_common(20)
+corpus_most_common = counts_from_corpus.most_common(10)
+t0  = set([item[0] for item in corpus_most_common])
 
 if True:
-    for index, item in enumerate(t0):
+    for index, item in enumerate(corpus_most_common):
         logging.debug('%s: %s :: %d' % (index + 1, item[0], item[1]))
-    most_words = set([each[0] for each in t0])
+    most_words = set([each[0] for each in corpus_most_common])
     logging.debug(most_words)
     for item in counts_from_documents:
         t1 = item.most_common(10)
         current = set([each[0] for each in t1])
-        logging.debug(current.difference(t0))
+        difference  = t0.difference(current)
+        logging.debug('%d : %s' % (len(difference), difference))
