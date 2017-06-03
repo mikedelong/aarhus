2d65094f2efe62a7f155046238888afed0fb5939
Mike DeLong <mikedelong@outlook.com>
2017-06-03 19:10:42 -0400

    feature: added the intersection and difference with their lengths.

--- aarhus/read_frequencies.py
+++ aarhus/read_frequencies.py
@@ -1,6 +1,7 @@
 import logging
 import pickle
 import collections
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s :: %(message)s', level=logging.DEBUG)
 
 logging.debug('started.')
@@ -26,15 +27,17 @@ if 'counts_from_documents' in data.keys():
     counts_from_documents = data['counts_from_documents']
 
 corpus_most_common = counts_from_corpus.most_common(10)
-t0  = set([item[0] for item in corpus_most_common])
+t0 = set([item[0] for item in corpus_most_common])
 
 if True:
     for index, item in enumerate(corpus_most_common):
         logging.debug('%s: %s :: %d' % (index + 1, item[0], item[1]))
     most_words = set([each[0] for each in corpus_most_common])
     logging.debug(most_words)
-    for item in counts_from_documents:
-        t1 = item.most_common(10)
+    for index, item in enumerate(counts_from_documents):
+        t1 = item.most_common(15)
         current = set([each[0] for each in t1])
-        difference  = t0.difference(current)
-        logging.debug('%d : %s' % (len(difference), difference))
+        intersection = t0.intersection(current)
+        difference = t0.difference(current)
+        logging.debug('%d : intersection : %d %s :: difference : %d %s' % (
+        index, len(intersection), intersection, len(difference), difference))
