22811bb1f88068078307491fd86a3880da4ac18b
Mike DeLong <mikedelong@outlook.com>
2017-06-24 20:50:45 -0400

    feature: initial imports, basic logging.

---
+++ aarhus/get_roots.py
@@ -0,0 +1,9 @@
+import logging
+import sys
+
+# http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s :: %(message)s', level=logging.DEBUG)
+
