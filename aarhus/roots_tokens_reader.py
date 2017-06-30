7188e2787953f833dc5bc0faaf9aaf0dad29e013
Mike DeLong <mikedelong@outlook.com>
2017-06-29 21:43:35 -0400

    feature: shell with logging and total runtime.

---
+++ aarhus/roots_tokens_reader.py
@@ -0,0 +1,24 @@
+
+import logging
+import pickle
+import sys
+import time
+
+# http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s :: %(message)s', level=logging.DEBUG)
+
+def run():
+    start_time = time.time()
+    logging.debug('started.')
+
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logging.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+
+
+if __name__ == '__main__':
+    run()
